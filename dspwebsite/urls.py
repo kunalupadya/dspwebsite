942d4ee158e85115e27efb5864dd892d3f49bd0d
Kunal Upadya <ku16@duke.edu>
2019-08-05 19:02:00 -0400

    fixed hosts

--- dspwebsite/urls.py
+++ dspwebsite/urls.py
@@ -17,5 +17,5 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('', include('mainwebsite.urls')),
+    path('/', include('mainwebsite.urls')),
 ]
