9005b17240f55896e7a85c20f67f4486e96836cb
Kunal Upadya <ku16@duke.edu>
2019-08-05 19:30:43 -0400

    fixed hosts

--- dspwebsite/settings.py
+++ dspwebsite/settings.py
@@ -132,6 +132,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'dspwebsite.storage.WhiteNoiseStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
