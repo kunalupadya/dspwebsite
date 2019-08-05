1efb0ffa29696ec82657bbdee7c9d5dcb78d9c1e
Kunal Upadya <ku16@duke.edu>
2019-08-05 19:38:16 -0400

    fixed hosts

--- dspwebsite/settings.py
+++ dspwebsite/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'xarz@-t1pqyt)$x3t^=7sfi8$#q!d60!0zk)du=(##z%y8(yau'
 DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = ['*']#'gentle-fortress-57854.herokuapp.com/',
-                 #'127.0.0.1']
+ALLOWED_HOSTS = ['gentle-fortress-57854.herokuapp.com/',
+                 '127.0.0.1']
 
 
 # Application definition
